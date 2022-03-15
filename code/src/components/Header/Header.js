8c27b306ff2a575cdb58129366d1d4adceb430ed
Nagarjun Enumudi Venkatesha <nagarjunev.11@gmail.com>
2022-03-14 22:11:13 -0500

    header changed

--- code/src/components/Header/Header.js
+++ code/src/components/Header/Header.js
@@ -1,14 +1,62 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Anchor,
+  Span,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <Anchor>
+          <DiCssdeck size="3rem" />
+          <Span>Portfolio</Span>
+        </Anchor>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#technologies">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://www.linkedin.com/in/nagev/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.github.com/nagarjunEV">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.twitter.com/nagarjunev_11">
+        <AiFillTwitterCircle size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
