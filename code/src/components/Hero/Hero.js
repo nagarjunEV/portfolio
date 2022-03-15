824917936412acf3b2b51a0aad56e309955165e5
Nagarjun Enumudi Venkatesha <nagarjunev.11@gmail.com>
2022-03-14 22:20:41 -0500

    heading section changes

--- code/src/components/Hero/Hero.js
+++ code/src/components/Hero/Hero.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionSubText,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello World!
+      </SectionTitle>
+      <SectionText>Lorem Ipsum</SectionText>
+      <Button>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
